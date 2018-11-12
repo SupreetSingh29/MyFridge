d7c7ff74a5a65d252c48254bb030acf2e4b9f1d0
SupreetSingh29 <sam29sept@gmail.com>
2018-11-11 20:52:30 -0500

    Assignment3

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var controllerFood = require('../controllers/food');
+// API Routes
+router.get('/food', controllerFood.get_food);
+router.get('/food/:id', controllerFood.foodById);
+router.post('/food', controllerFood.post_food);
+router.delete('/food/:id', controllerFood.foodDelete);
+router.put('/food/:id', controllerFood.put_food);
 
 module.exports = router;
